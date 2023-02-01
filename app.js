b87827b2b65985b3d7c250d240755e8516dc67d9
Subha Sarkar <sarkarsubha1206@gmail.com>
2023-02-01 22:37:44 +0530

    updated code to run in every 5 min

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const transactionalEmailApi = new Sib.TransactionalEmailsApi();
 
 let alreadySent = false;
 
-cron.schedule(`*/15 * * * *`, async () => {
+cron.schedule(`*/5 * * * *`, async () => {
     const res = await axios.get(process.env.CHECK_URL);
     if (res.status !== 200 && !alreadySent) {
         transactionalEmailApi
